6ead7a149c872baea6e16e7d01aa7ef4830a385f
someqst <nik.som201@mail.ru>
2026-02-26 23:00:38 +0300

    fix race condition in single uow

--- src/services/services_factory.py
+++ src/services/services_factory.py
@@ -5,15 +5,12 @@ from utils.unit_of_work import UnitOfWork
 
 class ServicesFactory:
     def __init__(self):
-        self._uow = UnitOfWork()
         self._message_service: MessageService = None
         self._ai_service: AIService = None
 
     @property
-    def message(self):
-        if not self._message_service:
-            self._message_service = MessageService(self._uow)
-        return self._message_service
+    def message(self) -> MessageService:
+        return MessageService(UnitOfWork())
     
     @property
     def ai(self):
